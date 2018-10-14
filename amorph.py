a463d8bdccafd54e92167ffeed474c2168fe0ab7
Daniel B <busisd@carleton.edu>
2018-10-14 00:27:45 -0500

    Biters added!

--- amorph.py
+++ amorph.py
@@ -12,6 +12,7 @@ class AmorphGameController():
 	def __init__(self):
 		self.enemy_group = pygame.sprite.Group()
 		self.big_enemy_group = pygame.sprite.Group()
+		self.biter_enemy_group = pygame.sprite.Group()
 		self.player_group = pygame.sprite.GroupSingle()
 		self.player = PlayerSprite()
 		self.player_group.add(self.player)
@@ -139,7 +140,6 @@ class BigGreenSprite(pygame.sprite.Sprite):
 				target_angle = math.pi
 		else:
 			target_angle = math.atan2(y_dif, x_dif)
-				
 		target_angle = self._normify_angle(target_angle)
 		
 		angle_dif = self._normify_angle(target_angle - self.angle)
@@ -187,6 +187,130 @@ class BigGreenSprite(pygame.sprite.Sprite):
 				start_pos_x = self.w
 			return [start_pos_x, start_pos_y]
 
+			
+			
+class BiterSprite(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.size = (40,40)
+		self.radius = 19
+		
+		self.death_event = pygame.event.Event(pygame.USEREVENT, {"descript":"biter_death"})
+		
+		self.angle = 2*math.pi*random.random()
+		self.rotate_speed = math.pi/150
+		
+		self.w, self.h = pygame.display.get_surface().get_size()
+		
+		self.pos = self._random_start_pos()
+		self.rect = pygame.Rect(self.pos, self.size)
+		
+		self.image = pygame.Surface(self.size, pygame.SRCALPHA)
+		self.color_chase = pygame.Color(0,0,random.randrange(100,200))
+		self.color_lunge = pygame.Color(random.randrange(100,200),0,0)
+		pygame.gfxdraw.filled_circle(self.image, int(self.size[0]/2), int(self.size[0]/2), self.radius, self.color_chase)
+		
+		self.is_lunging = False
+		self.lunge_cur_ticks = 0
+		self.lunge_max_ticks = 60
+		self.lunge_speed = 4.5
+		self.chase_speed = 2.6
+		self.lunge_max_dist = self.lunge_max_ticks*self.lunge_speed
+		
+	def update(self, player_pos):
+		if self.is_lunging:
+			self.update_lunge(player_pos)
+		else:
+			self.update_chase(player_pos)
+	
+	def update_lunge(self, player_pos):
+		direction = [math.cos(self.angle), math.sin(self.angle)]
+		self.pos[0] += self.lunge_speed*direction[0]
+		self.pos[1] += self.lunge_speed*direction[1]
+		self.lunge_cur_ticks += 1
+		if self.lunge_cur_ticks > self.lunge_max_ticks:
+			self.lunge_cur_ticks = 0
+			self.is_lunging = False
+			pygame.gfxdraw.filled_circle(self.image, int(self.size[0]/2), int(self.size[0]/2), self.radius, self.color_chase)
+		if (self.pos[0] > self.w + self.size[0]/2
+				or self.pos[1]>self.h + self.size[1]/2
+				or self.pos[0]<-self.size[0]/2
+				or self.pos[1]<-self.size[1]/2):
+			pygame.event.post(self.death_event)
+			self.kill()
+		self.rect = pygame.Rect((self.pos[0]-self.size[0]/2, self.pos[1]-self.size[1]/2), self.size)	
+	
+	def update_chase(self, player_pos):
+		x_dif = player_pos[0] - self.pos[0] 
+		y_dif =  player_pos[1] - self.pos[1]
+		if x_dif == 0 and y_dif == 0:
+			target_angle = 0
+		elif x_dif == 0:
+			if y_dif > 0:
+				target_angle = math.pi/2
+			else:
+				target_angle = math.pi*3/2
+		elif y_dif == 0:
+			if x_dif > 0:
+				target_angle = 0
+			else:
+				target_angle = math.pi
+		else:
+			target_angle = math.atan2(y_dif, x_dif)
+				
+		target_angle = self._normify_angle(target_angle)
+		
+		angle_dif = self._normify_angle(target_angle - self.angle)
+		if abs(angle_dif) < self.rotate_speed:
+			self.angle = target_angle
+			
+			dist_to_player = (x_dif**2+y_dif**2)**(1/2)
+			if dist_to_player < self.lunge_max_dist:
+				self.is_lunging = True
+				pygame.gfxdraw.filled_circle(self.image, int(self.size[0]/2), int(self.size[0]/2), self.radius, self.color_lunge)
+		else:
+			if angle_dif < math.pi:
+				self.angle += self.rotate_speed
+			else:
+				self.angle -= self.rotate_speed
+			self.angle = self._normify_angle(self.angle)
+		
+		direction = [math.cos(self.angle), math.sin(self.angle)]
+		self.pos[0] += self.chase_speed*direction[0]
+		self.pos[1] += self.chase_speed*direction[1]
+		
+		if (self.pos[0] > self.w + self.size[0]/2
+				or self.pos[1]>self.h + self.size[1]/2
+				or self.pos[0]<-self.size[0]/2
+				or self.pos[1]<-self.size[1]/2):
+			pygame.event.post(self.death_event)
+			self.kill()
+
+		self.rect = pygame.Rect((self.pos[0]-self.size[0]/2, self.pos[1]-self.size[1]/2), self.size)	
+
+	def _normify_angle(self, angle):
+		if angle > 2*math.pi:
+			angle -= 2*math.pi
+		if angle < 0:
+			angle += 2*math.pi
+		return angle
+	
+	def _random_start_pos(self):
+		direction = [math.cos(self.angle), math.sin(self.angle)]
+		horiz_edge = bool(random.getrandbits(1))
+		if horiz_edge:
+			start_pos_x = random.randrange(self.w+1)
+			start_pos_y = 0
+			if direction[1] < 0:
+				start_pos_y = self.h
+			return [start_pos_x, start_pos_y]
+		else:
+			start_pos_y = random.randrange(self.h+1)
+			start_pos_x = 0
+			if direction[0] < 0:
+				start_pos_x = self.w
+			return [start_pos_x, start_pos_y]
+
 def main():
 	pygame.init()	
 	
@@ -195,8 +319,8 @@ def main():
 	
 	for i in range(10):
 		game_control.enemy_group.add(GreenSprite())
-	
 	game_control.big_enemy_group.add(BigGreenSprite())
+	game_control.biter_enemy_group.add(BiterSprite())
 	
 	running = True
 	while(running):
@@ -215,12 +339,16 @@ def main():
 					game_control.enemy_group.add(GreenSprite())
 				if event.descript=="big_blob_death":
 					game_control.big_enemy_group.add(BigGreenSprite())
+				if event.descript=="biter_death":
+					game_control.big_enemy_group.add(BiterSprite())
+
 
 		_check_blob_bounces(game_control)
 		_check_big_small_bounces(game_control)
 		game_control.player_group.update()
 		game_control.enemy_group.update()
 		game_control.big_enemy_group.update(game_control.player_group.sprite.pos)
+		game_control.biter_enemy_group.update(game_control.player_group.sprite.pos)
 		_update_screen(game_control)
 		game_control.game_clock.tick(60)
 	pygame.quit()
@@ -229,6 +357,7 @@ def _update_screen(game_control):
 	game_control.screen.fill(game_control.bg_color)
 	game_control.enemy_group.draw(game_control.screen)
 	game_control.big_enemy_group.draw(game_control.screen)
+	game_control.biter_enemy_group.draw(game_control.screen)
 	game_control.player_group.draw(game_control.screen)
 	#print(pygame.sprite.spritecollide(game_control.player_group.sprite, game_control.enemy_group, False, collided=pygame.sprite.collide_circle))
 	pygame.display.update()
